9370a67aa723adb84e7135b2c889379308fcd67f
Thomas James <42183357+ThomasJames@users.noreply.github.com>
2020-04-10 17:32:30 +0100

    Adjusted for sentinel-2 only

--- Spectral_Indicies.py
+++ Spectral_Indicies.py
@@ -1,25 +1,25 @@
 import numpy as np
 
-class SpectralIndex:
+class S2SpectralIndex:
     def __init__(self,
-                 s2band1, s2band2, s2band3, s2band4, s2band5,
-                 s2band6, s2band7, s2band8, s2band9, s2band10,
-                 s2band11, s2band12, s2band13):
+                 band1, band2, band3, band4, band5,
+                 band6, band7, band8, band9, band10,
+                 band11, band12, band13):
 
         # Copernicus Sentinel 2
-        self.ultra_blue = s2band1
-        self.blue = s2band2
-        self.green = s2band3
-        self.re1 = s2band4
-        self.re2 = s2band5
-        self.re3 = s2band6
-        self.NIR = s2band7
-        self.NIRn = s2band8
-        self.Water_vapor = s2band9
-        self.SWIR_cirrus = s2band10
-        self.SWIR1 = s2band11
-        self.SWIR2 = s2band12
-        self.band12 = s2band13
+        self.ultra_blue = band1
+        self.blue = band2
+        self.green = band3
+        self.re1 = band4
+        self.re2 = band5
+        self.re3 = band6
+        self.NIR = band7
+        self.NIRn = band8
+        self.Water_vapor = band9
+        self.SWIR_cirrus = band10
+        self.SWIR1 = band11
+        self.SWIR2 = band12
+        self.band12 = band13
 
     """
     normalized difference built-up index 
